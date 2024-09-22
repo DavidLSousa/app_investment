7f40cb82eb25c0194a62234a6a675c9eaa1fee76
davidsousa <davidlsousa.dev@gmail.com>
2024-09-22 01:09:54 -0300

    implementing includes with jinja

--- controller/ticket_controller.py
+++ controller/ticket_controller.py
@@ -8,11 +8,11 @@ class TicketController:
   @classmethod
   def render_add_page(cls):
     # Pegar info do db e passar para renderizar a pagina com os dados dos tickets do db
-    return render_template('add_tickets_page.html')
+    return render_template('add_tickets_page.html', title_page='Adicionar Ativos')
 
   @classmethod
   def render_all_page(cls):
-    return render_template('all_tickets_page.html')
+    return render_template('all_tickets_page.html', title_page='Meus Ativos')
 
   @classmethod
   def add_ticket_controller(cls):
