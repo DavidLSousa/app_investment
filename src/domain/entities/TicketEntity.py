c8443aa339d79d960d7550be4bb03037b795f188
davidsousa <davidlsousa.dev@gmail.com>
2024-09-23 16:56:22 -0300

    implementing entities

---
+++ src/domain/entities/TicketEntity.py
@@ -0,0 +1,38 @@
+from dataclasses import dataclass
+
+@dataclass
+class TicketEntity:
+  _nameTicket: str
+  _ticket: str
+  _number_of_tickets: int
+  _total_value_purchased: float
+  _highest_price: float
+  _lowest_price: float
+  _average_price: float
+  _history: list[dict]
+
+  @property
+  def number_of_tickets(self):
+    return self._number_of_tickets
+
+  @number_of_tickets.setter
+  def number_of_tickets(self, value):
+    if (value > 0):
+      self._number_of_tickets = value
+      return
+    
+    raise ValueError("O número de tickets deve ser maior que 0")
+    
+  @property
+  def total_value_purchased(self):
+    return self._total_value_purchased
+
+  @total_value_purchased.setter
+  def total_value_purchased(self, value):
+    if (value > 0):
+      self._total_value_purchased = value
+      return
+    
+    raise ValueError("O valor total de compra deve ser maior que 0")
+    
+    
