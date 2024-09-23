107037db4d153da3d98ddb8a0f91490c094715c7
davidsousa <davidlsousa.dev@gmail.com>
2024-09-23 18:36:52 -0300

    feat(interface): implement TicketInterface

---
+++ src/domain/interfaces/ticket_interface.py
@@ -0,0 +1,24 @@
+from abc import ABC, abstractmethod
+
+from src.domain.entities.ticket_entity import TicketEntity
+
+class TicketInterface(ABC):
+  @abstractmethod
+  def get_ticket(self, ticket_id: int):
+    pass
+
+  @abstractmethod
+  def get_all_ticket(self):
+    pass
+
+  @abstractmethod
+  def create_ticket(self, ticket: TicketEntity):
+    pass
+
+  @abstractmethod
+  def update_ticket(self, ticket_id: int, ticket: TicketEntity):
+    pass
+
+  @abstractmethod
+  def delete_ticket(self, ticket_id: int):
+    pass
