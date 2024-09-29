88ad9d6dfed566006c0f479d3172155033178ebc
davidsousa <davidlsousa.dev@gmail.com>
2024-09-29 16:17:07 -0300

    implementing return for each func and implementing get_all_ticket in service

--- src/domain/services/mysql_services.py
+++ src/domain/services/mysql_services.py
@@ -8,7 +8,7 @@ from models import Ticket
 
 @dataclass
 class MysqlServices(TicketInterface):
-  def get_ticket(self, ticket_name: str):
+  def get_ticket(self, ticket_name: str) -> TicketEntity | None:
     try:
       query = Ticket.select().where(Ticket.ticket == ticket_name)
       ticket = query.dicts().get()
@@ -18,35 +18,42 @@ class MysqlServices(TicketInterface):
     except Exception as err:
       return None
       
-  def get_all_ticket(self):
-    pass
+  def get_all_ticket(self) -> list[TicketEntity] | None: # Implementei sem testar
+    try:
+      query = Ticket.select()
+      tickets = query.dicts().get()
+      
+      return tickets
+    
+    except Exception as err:
+      return None
 
-  def create_ticket(self, ticket: TicketEntity):
+  def create_ticket(self, ticket: TicketEntity) -> None:
     Ticket.create(
-      nameTicket = ticket._nameTicket,
-      ticket = ticket._ticket,
-      number_of_tickets = ticket._number_of_tickets,
+      nameTicket =            ticket._nameTicket,
+      ticket =                ticket._ticket,
+      number_of_tickets =     ticket._number_of_tickets,
       total_value_purchased = ticket._total_value_purchased,
-      highest_price = ticket._highest_price,
-      lowest_price = ticket._lowest_price,
-      average_price = ticket._average_price,
-      history = ticket._history
+      highest_price =         ticket._highest_price,
+      lowest_price =          ticket._lowest_price,
+      average_price =         ticket._average_price,
+      history =               ticket._history
     )
   
-  def update_ticket(self, ticket_id: int, ticket: TicketEntity):
+  def update_ticket(self, ticket_id: int, ticket: TicketEntity) -> None:
     pass
   
-  def update_ticket_increment(self, ticket: TicketEntity):
+  def update_ticket_increment(self, ticket: TicketEntity) -> None:
     Ticket.update(
-      number_of_tickets = ticket._number_of_tickets,
+      number_of_tickets =     ticket._number_of_tickets,
       total_value_purchased = ticket._total_value_purchased,
-      highest_price = ticket._highest_price,
-      lowest_price = ticket._lowest_price,
-      average_price = ticket._average_price,
-      history = ticket._history
+      highest_price =         ticket._highest_price,
+      lowest_price =          ticket._lowest_price,
+      average_price =         ticket._average_price,
+      history =               ticket._history
     ).where(
       Ticket.ticket == ticket._ticket
       ).execute()
   
-  def delete_ticket(self, ticket_id: int):
+  def delete_ticket(self, ticket_id: int) -> None:
     pass
