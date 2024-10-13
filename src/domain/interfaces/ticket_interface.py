aeae809c0e58563d531081d15c793d51ebfb21ec
davidsousa <davidlsousa.dev@gmail.com>
2024-10-13 12:25:49 -0300

    remove comments for the last commit

--- src/domain/interfaces/ticket_interface.py
+++ src/domain/interfaces/ticket_interface.py
@@ -25,29 +25,3 @@ class TicketInterface(ABC):
     @abstractmethod
     def delete_ticket(self, ticket_name: str) -> None:
         pass
-
-# class TicketInterface(ABC):
-#     @abstractmethod
-#     def get_ticket_by_name(self, ticket_name: str) -> TicketEntity:
-#         """Retrieve a ticket by its name."""
-
-#     @abstractmethod
-#     def get_all_tickets(self) -> list[TicketEntity]:
-#         """Retrieve all tickets."""
-
-#     @abstractmethod
-#     def create_ticket(self, ticket: TicketEntity) -> None:
-#         """Create a new ticket."""
-
-#     @abstractmethod
-#     def update_ticket_after_sale(self, data_updated: TicketEntity) -> None:
-#         """Update a ticket after a sale."""
-
-#     @abstractmethod
-#     def update_ticket_after_increment(self, ticket: TicketEntity) -> None:
-#         """Update a ticket after an increment."""
-
-#     @abstractmethod
-#     def delete_ticket_by_id(self, ticket_id: int) -> None:
-#         """Delete a ticket by its id."""
-
